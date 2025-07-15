c2ce57c1ccb6dfe41ca384bb53a5c32af2621752
chenwell <chenwell1982@gmail.com>
2025-07-15 23:00:12 +0800

    [add]三消逻辑

--- Mahjongg/assets/Code/Mahjiong_Slot.ts
+++ Mahjongg/assets/Code/Mahjiong_Slot.ts
@@ -1,18 +1,44 @@
 import { _decorator, Component, Node, tween, Vec3 } from 'cc';
 import { Mahjiong_Prefab } from './Mahjiong_Prefab';
+import { Mahjiong_Init } from './Mahjiong_Init';
+import { Mahjiong_Click } from './Mahjiong_Click';
 const { ccclass, property } = _decorator;
 
 @ccclass('Mahjiong_Slot')
 export class Mahjiong_Slot extends Component {
     //卡槽管理脚本
+
+    //定时调度(延迟执行)：this.schedule(回调函数,延迟时间,重复次数)
+    //this.schedule(()=>{函数体},延迟时间,重复次数)
+
+    @property(Node)//导入消除动画节点
+    Boom: Node = null
+
     Node_List = []//2D麻将节点列表
+
+    Pos = null//动画节点位置
+
     start() {
 
     }
     Get_Node(Node: Node) {//接收麻将节点
         this.Set_Order(Node)//设置排列顺序
-        //this.Node_List.push(Node)//追加麻将到列表内
+        this.node.getComponent(Mahjiong_Click).off()//关闭监听：防止用户快速点击
         this.Run_Order()//开始排列
+        this.schedule(() => {//定时调度(延迟执行)
+            this.Clear_OK(this.Run_Clear(Node))//消除判断
+        }, 0.1)
+    }
+    Clear_OK(num: number) {//消除成功回调函数
+        if (num == 1) {//如果麻将不足3个
+            this.node.getComponent(Mahjiong_Click).on()//开启监听
+        } else if (num == 2) {//如果麻将不足3个
+            this.node.getComponent(Mahjiong_Click).on()//开启监听
+        } else {//如果是三消
+            this.Clear_Boom()//播放消除动画
+            this.Run_Order()//重新排列
+            this.node.getComponent(Mahjiong_Click).on()//开启监听
+        }
     }
     /* 
     设置排列顺序 --- 三消游戏中经典的相邻算法
@@ -45,6 +71,44 @@ export class Mahjiong_Slot extends Component {
         }
     }
 
+    Run_Clear(node: Node) {//消除函数 ******* 三消经典算法
+        if (this.Node_List.length < 3) {//如果列表内的麻将数量小于3
+            return 1//退出函数
+        }
+        let lie = []//创建临时空列表，用于存放相同麻将节点
+        const num = node.getComponent(Mahjiong_Prefab).Num//获取当前麻将的编号
+        for (let x of this.Node_List) {//遍历列表
+            if (num == x.getComponent(Mahjiong_Prefab).Num) {//如果当前麻将编号与新麻将编号相同
+                lie.push(x)//把当前麻将追加到临时列表
+            }
+        }
+        if (lie.length < 3) {//如果临时列表内的麻将数量小于3
+            return 2//退出函数
+        }
+        for (let i = 0; i < 3; i++) {//遍历临时列表
+            const x = lie[i]//获取相同麻将节点
+            this.node.getComponent(Mahjiong_Init).Mahjiong_NodePool.put(x)//回收麻将节点
+            this.Node_List = this.Node_List.filter(item => item != x)//返回不等于当前节点的列表
+        }
+        this.Pos = lie[1].position//获取消除的麻将，中间麻将的位置
+        return 3//消除后返回，表示已经消除成功
+    }
+
+    Clear_Boom() {//消除动画
+        let Scale = 0.1//初始缩放系数
+        this.Boom.setPosition(this.Pos)//把动画节点移动到消除的中间麻将的位置
+        this.Boom.setScale(Scale, Scale, Scale)//设置缩放
+        this.Boom.active = true//显示动画节点
+        const boom = () => {//动画执行内部函数
+            Scale += 0.1//自增缩放
+            this.Boom.setScale(Scale, Scale, Scale)//设置缩放
+            if (Scale >= 0.8) {//如果缩放系数大于1.2
+                this.Boom.active = false//隐藏动画节点
+                this.unschedule(boom)//取消定时调度
+            }
+        }
+        this.schedule(boom, 0.01)//定时调度执行动画
+    }
     update(deltaTime: number) {
 
     }
